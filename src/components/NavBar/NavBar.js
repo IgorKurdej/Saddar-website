a907e5b8ec2c0e6a0cba94cab251b21128aa08b8
IGOR <qrdej31@gmail.com>
2022-03-11 23:05:50 +0100

    commit

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,22 +1,27 @@
-import React from 'react';
-import * as S from './Navbar.style';
+import React from "react";
+import * as S from "./Navbar.style";
+import { Link } from "react-router-dom";
 
-const NavBar = ({isHomePage, isScrollMove}) => {
-    return (
-        <S.Wrapper isHomePage={isHomePage} isScrollMove={isScrollMove}>
-            <S.Ul>
-                <S.Li>O nas</S.Li>
-                <S.Li>Nasze specjalizacje</S.Li>
-                <S.Li>Know-how</S.Li>
-                <S.Li>Produkty</S.Li>
-                <S.Li>Usługi</S.Li>
-                <S.Li>Partnerzy handlowi</S.Li>
-                <S.Li>Aktualności</S.Li>
-                <S.Li>Instrukcja</S.Li>
-                <S.Li>Kontakt</S.Li>
-            </S.Ul>
-        </S.Wrapper>
-    );
+const NavBar = ({ isHomePage, isScrollMove }) => {
+  return (
+    <S.Wrapper isHomePage={isHomePage} isScrollMove={isScrollMove}>
+      <S.Ul>
+        <S.Li>
+          <Link to="/about">O nas</Link>
+        </S.Li>
+        <S.Li>Nasze specjalizacje</S.Li>
+        <S.Li>Know-how</S.Li>
+        <S.Li>
+          <Link to="/products">Produkty</Link>
+        </S.Li>
+        <S.Li>Usługi</S.Li>
+        <S.Li>Partnerzy handlowi</S.Li>
+        <S.Li>Aktualności</S.Li>
+        <S.Li>Instrukcja</S.Li>
+        <S.Li>Kontakt</S.Li>
+      </S.Ul>
+    </S.Wrapper>
+  );
 };
 
 export default NavBar;
