6cca3343079c2f4e82552a5fbd599ee80f2e9222
IGOR <qrdej31@gmail.com>
2022-03-11 22:55:17 +0100

    ref

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import HomeView from "./views/HomeView";
 import "./App.css";
 import AboutUsView from "./views/AboutUsView";
