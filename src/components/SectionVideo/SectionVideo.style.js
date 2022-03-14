c515ff43e7cfb579f0f50c2953c02a6882ea841a
IGOR <qrdej31@gmail.com>
2022-03-14 12:35:57 +0100

    refactoring video component

--- src/components/SectionVideo/SectionVideo.style.js
+++ src/components/SectionVideo/SectionVideo.style.js
@@ -1,15 +1,15 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Wrapper = styled.div`
-  background-color: #45454d;
+  //background-color: #45454d;
   margin-bottom: 100px;
   //height: calc(100vh - 80px);
   height: 100vh;
-`
+`;
 
 export const Video = styled.video`
   height: 100%;
   object-fit: cover;
   width: 100%;
   //border-radius: 150px;
-`
+`;
